fce314dbb153e74fa8080f547b325c0208618bd3
PaulDeckert <p.dckrt@gmail.com>
2024-04-12 21:12:37 +0200

    a

---
+++ Explosion.gd
@@ -0,0 +1,25 @@
+extends Area2D
+@onready var timer = $Timer
+@export var damage = 5
+var hit =false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+
+	timer.timeout.connect(func():
+		queue_free()
+	)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var node2ds = get_overlapping_bodies()
+	if hit:
+		return
+	for node in node2ds:
+		if node is Player:
+			node.damage(damage)
+			hit = true
+	
+	
--- bullet.gd
+++ bullet.gd
@@ -4,6 +4,8 @@ extends RigidBody2D
 @onready var kill_timer = $KillTimer
 @onready var collision = $CollisionShape2D
 
+const ExplosionScene = preload ("./explosion.tscn")
+
 func _ready():
 	collision.disabled = true
 	timer.timeout.connect(func():
@@ -18,6 +20,9 @@ func _physics_process(delta):
 	var collisions = get_colliding_bodies()
 
 	if collisions.size() > 0:
+		var explosion = ExplosionScene.instantiate()
+		get_parent().add_child(explosion)
+		explosion.global_position = global_position
 		queue_free()
 
 	for coll in collisions:
